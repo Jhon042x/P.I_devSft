e9d879fc2f4c1e6f1ce5feb7c30feddb307afca8
Jhon042x <jhonparra1245@gmail.com>
2025-05-08 23:31:32 -0500

    test 2

--- db_connection.py
+++ db_connection.py
@@ -3,12 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
 }
 
-SQLALCHEMY_DATABASE_URL = ("postgresql://p_i_devsft_user:lWFXVUGa62sOw6dwq7m67cwMd4ftBzlB@dpg-d0enlhumcj7s7380n7ig-a.virginia-postgres.render.com/p_i_devsft")
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
 def get_engine():
     return create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
